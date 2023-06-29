62b6d40c942d2f4c6dfbaaa7f8af7f0099cf2ec8
tomo0425 <onishi.tomoki@g.wakayama-u.jp>
2023-06-29 11:05:38 +0900

    user_add

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\FirstController;
 use App\Http\Controllers\ComponentController; 
+
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +12,8 @@ use App\Http\Controllers\ComponentController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -20,6 +21,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/first', [FirstController::class, 'index']);   
-Route::get('/components', [ComponentController::class, 'index'])->name('index');  
-Route::get('/components/{component}', [ComponentController::class,'show'])->name('show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/first', [FirstController::class, 'index'])->name('first');  
+    Route::get('/components', [ComponentController::class, 'index'])->name('index');  
+    Route::get('/components/{component}', [ComponentController::class,'show'])->name('show');
+
+});
+
+require __DIR__.'/auth.php';
