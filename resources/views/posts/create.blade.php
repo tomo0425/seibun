177ad26203d68afba281f4c2b028df412f87de65
tomo0425 <onishi.tomoki@g.wakayama-u.jp>
2023-07-31 14:15:02 +0900

    add seibunhyouka

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -49,16 +49,17 @@
                                     </div>
             
                                     <div class="rate-form">
-                                      <input id="star5" type="radio"  value="5" name="post[stars]">
-                                      <label for="star5">★</label>
-                                      <input id="star4" type="radio" value="4" name="post[stars]">
-                                      <label for="star4">★</label>
-                                      <input id="star3" type="radio"  value="3" name="post[stars]">
-                                      <label for="star3">★</label>
-                                      <input id="star2" type="radio"  value="2" name="post[stars]">
-                                      <label for="star2">★</label>
-                                      <input id="star1" type="radio"  value="1" name="post[stars]">
-                                      <label for="star1">★</label>
+                                        <h6>成分評価</h6>
+                                        <input id="star5" type="radio"  value="5" name="post[stars]">
+                                        <label for="star5">★</label>
+                                        <input id="star4" type="radio" value="4" name="post[stars]">
+                                        <label for="star4">★</label>
+                                        <input id="star3" type="radio"  value="3" name="post[stars]">
+                                        <label for="star3">★</label>
+                                        <input id="star2" type="radio"  value="2" name="post[stars]">
+                                        <label for="star2">★</label>
+                                        <input id="star1" type="radio"  value="1" name="post[stars]">
+                                        <label for="star1">★</label>
                                     </div>
                                     
                                     <div class="row mb-3">
