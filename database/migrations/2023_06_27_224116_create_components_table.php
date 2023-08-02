3e0e2aac913d84f9a35a90481582ff9b2f8a327e
tomo0425 <onishi.tomoki@g.wakayama-u.jp>
2023-08-02 17:41:35 +0900

    change components table

--- database/migrations/2023_06_27_224116_create_components_table.php
+++ database/migrations/2023_06_27_224116_create_components_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('components', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
-            $table->string('effect', 200);
+            $table->text('effect');
             $table->timestamps();
             $table->softDeletes();
         });
